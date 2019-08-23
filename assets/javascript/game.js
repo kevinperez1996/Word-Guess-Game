68bfba8ba4cfa036ed8348eba01811e4c5949e67
Kevin Perez <kevinperez996@outlook.com>
2019-08-23 04:03:51 -0400

    added music element that will play for 20 seconds. also added all audio files and images. still working on small bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 
 //create array of words
-var artist = ["drake", "chane", "jcole"];
+var artist = ["drake", "chance", "jcole", "kanye", "jayz", "kendrick", "bigsean", "migos", 
+"logic", "50cent", "eminem", "cardi", "meek", "gambino", "nicki"];
 
 //Point Tracker
 var wins = 0;
@@ -12,7 +13,6 @@ var wrongletter = [];
 var lettersGuessed = [];
 var computerGuess = Math.floor(Math.random() * artist.length);
 var computerChoice = artist[computerGuess];
-
 function reset() {
 
     //this displays the current scores on the page, before any changes are made.
@@ -20,7 +20,7 @@ function reset() {
     document.getElementById("win-count").innerHTML = wins;
     document.getElementById("loss-count").innerHTML = losses;
 
-    
+
     //choose word randomly
     computerGuess = Math.floor(Math.random() * artist.length);
     computerChoice = artist[computerGuess];
@@ -30,14 +30,14 @@ function reset() {
     lettersGuessed = [];
     guesses = 10;
     console.log(computerChoice);
-    
+
     //create underscores for length of word
     var wordLength = (computerChoice.length);
     function makeUnderscore() {
         for (var i = 0; i < computerChoice.length; i++) {
             underScore.push("_")
             //Adds underscores in begining of game 
-             document.getElementById("underscores").innerHTML = underScore.join(" ");
+            document.getElementById("underscores").innerHTML = underScore.join(" ");
         }
         return underScore;
     }
@@ -59,18 +59,138 @@ document.onkeyup = function (event) {
         document.getElementById("underscores").innerHTML = underScore.join("");
 
         if (underScore.join("") === computerChoice && computerChoice === "drake") {
-            document.getElementById("cover").src = "assets/images/Drake.jpg";
+            document.getElementById("cover").src = "assets/images/drake.jpg";
+            var audio = new Audio('assets/audio/drake.mov');
+            audio.play();
+            document.getElementById("song-name").innerHTML = "Drake - Gold Roses";
             wins++;
             document.getElementById("win-count").textContent = wins;
+            
         }
         else if (underScore.join("") === computerChoice && computerChoice === "jcole") {
-            document.getElementById("cover").src = "assets/images/Jcole.jpg";
+            document.getElementById("cover").src = "assets/images/jcole.jpg";
             wins++;
+            var audio = new Audio('assets/audio/jcole.mov');
+            audio.play();
+            var audio = "";
+            document.getElementById("song-name").innerHTML = "Jcole - High for Hours";
             document.getElementById("win-count").textContent = wins;
         }
-        else if (underScore.join("") === computerChoice && computerChoice === "chane") {
-            document.getElementById("cover").src = "assets/images/Chance.jpg";
+        else if (underScore.join("") === computerChoice && computerChoice === "kanye") {
+            document.getElementById("cover").src = "assets/images/kanye.jpg";
             wins++;
+            var audio = new Audio('assets/audio/kanye.mov');
+            audio.play();
+            var audio = "";
+            document.getElementById("song-name").innerHTML = "Kanye - XTCY";
+            document.getElementById("win-count").textContent = wins;
+        }
+        else if (underScore.join("") === computerChoice && computerChoice === "jayz") {
+            document.getElementById("cover").src = "assets/images/jayz.jpg";
+            wins++;
+            var audio = new Audio('assets/audio/jayz.mov');
+            audio.play();
+            var audio = "";
+            document.getElementById("song-name").innerHTML = "Jay-Z - Top Off";
+            document.getElementById("win-count").textContent = wins;
+        }
+        else if (underScore.join("") === computerChoice && computerChoice === "kendrick") {
+            document.getElementById("cover").src = "assets/images/kendrick.jpg";
+            wins++;
+            var audio = new Audio('assets/audio/kendrick.mov');
+            audio.play();
+            var audio = "";
+            document.getElementById("song-name").innerHTML = "Kendrick Lamar  - Black Friday";
+            document.getElementById("win-count").textContent = wins;
+        }
+        else if (underScore.join("") === computerChoice && computerChoice === "bigsean") {
+            document.getElementById("cover").src = "assets/images/bigsean.jpg";
+            wins++;
+            var audio = new Audio('assets/audio/bigsean.mov');
+            audio.play();
+            var audio = "";
+            document.getElementById("song-name").innerHTML = "Big Sean - Overtime";
+            document.getElementById("win-count").textContent = wins;
+        }
+        else if (underScore.join("") === computerChoice && computerChoice === "migos") {
+            document.getElementById("cover").src = "assets/images/migos.jpg";
+            wins++;
+            var audio = new Audio('assets/audio/migos.mov');
+            audio.play();
+            var audio = "";
+            document.getElementById("song-name").innerHTML = "Migos - No Cap";
+            document.getElementById("win-count").textContent = wins;
+        }
+        else if (underScore.join("") === computerChoice && computerChoice === "logic") {
+            document.getElementById("cover").src = "assets/images/logic.jpg";
+            wins++;
+            var audio = new Audio('assets/audio/logic.mov');
+            audio.play();
+            var audio = "";
+            document.getElementById("song-name").innerHTML = "Logic - Wassup";
+            document.getElementById("win-count").textContent = wins;
+        }
+        else if (underScore.join("") === computerChoice && computerChoice === "50cent") {
+            document.getElementById("cover").src = "assets/images/50cent.jpg";
+            wins++;
+            var audio = new Audio('assets/audio/50cent.mov');
+            audio.play();
+            var audio = "";
+            document.getElementById("song-name").innerHTML = "50 Cent - Crazy";
+            document.getElementById("win-count").textContent = wins;
+        }
+        else if (underScore.join("") === computerChoice && computerChoice === "eminem") {
+            document.getElementById("cover").src = "assets/images/eminem.jpg";
+            wins++;
+            var audio = new Audio('assets/audio/eminem.mov');
+            audio.play();
+            var audio = "";
+            document.getElementById("song-name").innerHTML = "Eminem - Killshot";
+            document.getElementById("win-count").textContent = wins;
+        }
+        else if (underScore.join("") === computerChoice && computerChoice === "cardi") {
+            document.getElementById("cover").src = "assets/images/cardi.jpg";
+            wins++;
+            var audio = new Audio('assets/audio/cardi.mov');
+            audio.play();
+            var audio = "";
+            document.getElementById("song-name").innerHTML = "Cardi B - I Like It";
+            document.getElementById("win-count").textContent = wins;
+        }
+        else if (underScore.join("") === computerChoice && computerChoice === "meek") {
+            document.getElementById("cover").src = "assets/images/meek.jpg";
+            wins++;
+            var audio = new Audio('assets/audio/meek.mov');
+            audio.play();
+            var audio = "";
+            document.getElementById("song-name").innerHTML = "Meek Mill - Key to the Streets";
+            document.getElementById("win-count").textContent = wins;
+        }
+        else if (underScore.join("") === computerChoice && computerChoice === "gambino") {
+            document.getElementById("cover").src = "assets/images/gambino.jpg";
+            wins++;
+            var audio = new Audio('assets/audio/gambino.mov');
+            audio.play();
+            var audio = "";
+            document.getElementById("song-name").innerHTML = "Childish Gambino - Waiting for my Moment";
+            document.getElementById("win-count").textContent = wins;
+        }
+        else if (underScore.join("") === computerChoice && computerChoice === "nicki") {
+            document.getElementById("cover").src = "assets/images/nicki.jpg";
+            wins++;
+            var audio = new Audio('assets/audio/nicki.mov');
+            audio.play();
+            var audio = "";
+            document.getElementById("song-name").innerHTML = "Nicki Minaj - Megatron";
+            document.getElementById("win-count").textContent = wins;
+        }
+        else if (underScore.join("") === computerChoice && computerChoice === "chance") {
+            document.getElementById("cover").src = "assets/images/chance.jpg";
+            wins++;
+            var audio = new Audio('assets/audio/chance.mov');
+            audio.play();
+            var audio = "";
+            document.getElementById("song-name").innerHTML = "Chance the Rapper - The First Time";
             document.getElementById("win-count").textContent = wins;
         }
     }
@@ -87,12 +207,10 @@ document.onkeyup = function (event) {
         losses++;
         document.getElementById("loss-count").textContent = losses;
         document.getElementById("letters-count").innerHTML = "";
-        //document.getElementById("guess-count").innerHTML = 10;
         reset();
     }
     if (underScore.join("") === computerChoice) {
         document.getElementById("letters-count").innerHTML = "";
-        //document.getElementById("guess-count").inneHTML = 10;
         reset();
     }
 }
---
+++ assets/javascript/game2.js
@@ -0,0 +1,104 @@
+
+//create array of words
+var artist = ["drake", "chance", "jcole"];
+
+//Point Tracker
+var wins = 0;
+var losses = 0;
+var guesses = [];
+var underScore = [];
+var rightletter = [];
+var wrongletter = [];
+var lettersGuessed = [];
+var computerGuess = Math.floor(Math.random() * artist.length);
+var computerChoice = artist[computerGuess];
+
+function reset() {
+
+    //this displays the current scores on the page, before any changes are made.
+    document.getElementById("guess-count").innerHTML = 10;
+    document.getElementById("win-count").innerHTML = wins;
+    document.getElementById("loss-count").innerHTML = losses;
+
+
+    //choose word randomly
+    computerGuess = Math.floor(Math.random() * artist.length);
+    computerChoice = artist[computerGuess];
+    underScore = [];
+    rightletter = [];
+    wrongletter = [];
+    lettersGuessed = [];
+    guesses = 10;
+    console.log(computerChoice);
+
+    //create underscores for length of word
+    var wordLength = (computerChoice.length);
+    function makeUnderscore() {
+        for (var i = 0; i < computerChoice.length; i++) {
+            underScore.push("_")
+            //Adds underscores in begining of game 
+            document.getElementById("underscores").innerHTML = underScore.join(" ");
+        }
+        return underScore;
+    }
+    console.log(makeUnderscore());
+}
+reset();
+
+
+//record user guess
+document.onkeyup = function (event) {
+    var userInput = event.key;
+
+    //if the letter is in the word, we add the letter to "rightletter" array
+for (j=0; j<computerChoice.length; j++){
+    if (userInput===) {
+        underScore[j]=userInput;
+        rightletter.push(userInput);
+    
+        //replace underscore with correct letter
+        underScore[computerChoice.indexOf(userInput)] = userInput;
+        document.getElementById("underscores").innerHTML = underScore.join("");
+
+        if (underScore.join("") === computerChoice && computerChoice === "drake") {
+            document.getElementById("cover").src = "assets/images/Drake.jpg";
+            wins++;
+            document.getElementById("win-count").textContent = wins;
+            audioElement.setAttribute("src", "https://soundcloud.com/octobersveryown/drake-money-in-the-grave#t=0:15");
+
+        }
+        else if (underScore.join("") === computerChoice && computerChoice === "jcole") {
+            document.getElementById("cover").src = "assets/images/Jcole.jpg";
+            wins++;
+            document.getElementById("win-count").textContent = wins;
+        }
+        else if (underScore.join("") === computerChoice && computerChoice === "chane") {
+            document.getElementById("cover").src = "assets/images/Chance.jpg";
+            wins++;
+            document.getElementById("win-count").textContent = wins;
+        }
+    }
+}
+
+    //if the letter is not in the word we add it to the "wrong letter" array and we make sure we do not repeat used words 
+    if (computerChoice.indexOf(userInput) < 0 && wrongletter.indexOf(userInput) < 0) {
+        wrongletter.push(userInput);
+        lettersGuessed.push(userInput);
+        guesses--;
+        document.getElementById("guess-count").textContent = guesses;
+        document.getElementById("letters-count").textContent = lettersGuessed;
+
+    }
+    if (guesses === 0) {
+        losses++;
+        document.getElementById("loss-count").textContent = losses;
+        document.getElementById("letters-count").innerHTML = "";
+        //document.getElementById("guess-count").innerHTML = 10;
+        reset();
+    }
+    if (underScore.join("") === computerChoice) {
+        document.getElementById("letters-count").innerHTML = "";
+        //document.getElementById("guess-count").inneHTML = 10;
+        reset();
+    }
+}
